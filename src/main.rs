6e3df8433733d77355e9204f1e8d3f6c7034e420
aartoni <arto7796@gmail.com>
2022-06-07 13:26:30 +0200

    Add stream readers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
 use std::error::Error;
 
+use futures_util::{SinkExt, StreamExt};
+use serde_json::json;
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+
+const BINANCE_ADDRESS: &str = "wss://stream.binance.com:9443/ws/ethusd@depth10@1000ms";
+const BITSTAMP_ADDRESS: &str = "wss://ws.bitstamp.net";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
+    let ticker = String::from("ethbtc");
+    println!("Current ticker: {ticker}");
+
+    // Get Binance reader
+    let (stream, _) = connect_async(BINANCE_ADDRESS).await.unwrap();
+    let (_, binance_reader) = stream.split();
+
+    // Get Bitstamp reader and writer
+    let (stream, _) = connect_async(BITSTAMP_ADDRESS).await.unwrap();
+    let (mut writer, bitstamp_reader) = stream.split();
+
+    // Subscribe to Bitstamp order book stream
+    writer.send(Message::Text(json!({
+        "event": "bts:subscribe",
+        "data": {
+            "channel": "order_book_ethusd"
+        }
+    }).to_string())).await.unwrap();
+
+    loop {
+        // ...
+    }
 
     Ok(())
 }
